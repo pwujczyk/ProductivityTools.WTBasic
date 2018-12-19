15a3f163860d071414cc40c76876d79a3dec5017
Pawel Wujczyk <pwujczyk@hotmail.com>
2018-12-19 21:48:44 +0100

    display basic data

---
+++ single.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html	<?php	language_attributes();	?>>
+<head>
+	<meta charset="<?php	bloginfo('charset');	?>">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+	<title>
+		<?php bloginfo('name'); ?>
+	</title>
+		<header>
+			<div class="content">
+					<h1><?php bloginfo('name'); ?></h1>
+					<small><?php bloginfo('description'); ?></small>
+			</div>
+		</header>
+		<div class="content">
+			<?php if(have_posts()):	?>
+				<?php while(have_posts()): the_post(); ?>
+					<article class="post">
+						<h3>
+							<a href="<?php the_permalink(); ?>">
+								<?php the_title();?>
+							</a>
+						</h3>
+						<div class="meta">Created: <?php the_date('Y.m.d'); ?></div>
+						<?php the_content();?>
+					</article>
+				<?php endwhile;?>
+			<?php else: ?>
+				<?php echo wpautop('Sorry, No posts were found');?>
+			<?php	endif;	?>
+		</div>
+</head>
+</html>
