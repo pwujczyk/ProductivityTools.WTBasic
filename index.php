4c56efe1756453bdb7ed55600e97822900c9917c
Pawel Wujczyk <pwujczyk@hotmail.com>
2019-01-03 20:57:44 +0100

    load more and pagination

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html	<?php	language_attributes();	?>>
 <head>
-	<meta charset="<?php	bloginfo('charset');	?>">
+	<meta charset="<?php bloginfo('charset');	?>">
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+	<?php wp_head(); ?>
 	<title>
 		<?php bloginfo('name'); ?>
 	</title>
@@ -18,7 +19,10 @@
 				<?php	wp_nav_menu($args);	?>
 			</div>
 		</nav>
-		<div class="content">
+		
+</head>
+<body>
+	<div class="content" id="content">
 			<?php if(have_posts()):	?>
 				<?php while(have_posts()): the_post(); ?>
 					<article class="post">
@@ -30,10 +34,18 @@
 						<div class="meta">Created: <?php the_date('Y.m.d'); ?></div>
 						<?php the_excerpt(); ?>
 					</article>
-				<?php endwhile;?>
+					
+				<?php endwhile;?>			
 			<?php else: ?>
 				<?php echo wpautop('Sorry, No posts were found');?>
 			<?php	endif;	?>
-		</div>
-</head>
+			<div class="navigation"><p><?php posts_nav_link(); ?></p></div>
+			<div class="sidebar">
+				Test
+			</div>
+			<?php echo do_shortcode('[ajax_load_more container_type="div" post_type="post" scroll="false"]'); ?>
+
+	</div>
+	<?php wp_footer(); ?>
+</body>
 </html>
